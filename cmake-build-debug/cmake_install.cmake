0f2002dc5009befa8d2b3e0701c55fcc652dc73b
AlexanderSch12 <alexander.schoeters@gmail.com>
2022-10-31 15:15:22 +0100

    Initial Commit 2

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/alexa/Documents/KUL/BigData/Assignment1/src
+# Install script for directory: C:/Users/alexa/Documents/KUL/BigData/Assignment1/Assignment1_BigData
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/alexa/Documents/KUL/BigData/Assignment1/src/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/alexa/Documents/KUL/BigData/Assignment1/Assignment1_BigData/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
